9d371f5f9a5e3fb17cf5b46701e78d72e9661f1d
yusuf <bronot2021@gmail.com>
2026-01-20 01:45:10 +0530

    added robots

---
+++ src/app/robots.ts
@@ -0,0 +1,15 @@
+// app/robots.ts
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://flexpost.xyz'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/', // Block specific paths if needed
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
