11069d2957cefac8231f8aec55a4b5d4401638be
yusuf <bronot2021@gmail.com>
2026-01-20 01:41:43 +0530

    added sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://flexpost.xyz'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
